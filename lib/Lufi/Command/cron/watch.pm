722ba6708be50cf33438b82bed6290e808a3d758
Luc Didry <luc@didry.org>
2018-10-24 17:23:46 +0200

    Update cron commands default config

--- lib/Lufi/Command/cron/watch.pm
+++ lib/Lufi/Command/cron/watch.pm
@@ -21,9 +21,28 @@ sub run {
     my $config = $c->app->plugin('Config', {
         file    => $cfile,
         default => {
-            dbtype           => 'sqlite',
-            upload_dir       => 'files',
-            policy_when_full => 'warn'
+            prefix        => '/',
+            provisioning  => 100,
+            provis_step   => 5,
+            length        => 10,
+            token_length  => 32,
+            secrets       => ['hfudsifdsih'],
+            default_delay => 0,
+            max_delay     => 0,
+            mail          => {
+                how => 'sendmail'
+            },
+            mail_sender              => 'no-reply@lufi.io',
+            theme                    => 'default',
+            upload_dir               => 'files',
+            session_duration         => 3600,
+            allow_pwd_on_files       => 0,
+            dbtype                   => 'sqlite',
+            db_path                  => 'lufi.db',
+            force_burn_after_reading => 0,
+            x_frame_options          => 'DENY',
+            x_content_type_options   => 'nosniff',
+            x_xss_protection         => '1; mode=block',
         }
     });
 
