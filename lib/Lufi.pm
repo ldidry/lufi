abb9e4e4a25812bd4df47e8e1a04a3b15fe09da3
Luc Didry <luc@didry.org>
2019-07-30 20:17:52 +0200

    Avoid collision between startup and recurring provisionings

--- lib/Lufi.pm
+++ lib/Lufi.pm
@@ -72,9 +72,6 @@ sub startup {
         $self->provisioning();
     });
 
-    # For the first launch (after, this isn't really useful)
-    $self->provisioning();
-
     # Create directory if needed
     mkdir($self->config('upload_dir'), 0700) unless (-d $self->config('upload_dir'));
     die ('The upload directory ('.$self->config('upload_dir').') is not writable') unless (-w $self->config('upload_dir'));
