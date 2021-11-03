3962188200facf9bb101e55d1c5e75b5267264ec
Luc Didry <luc@didry.org>
2021-11-03 10:03:24 +0100

    🔒 Detect schemeless URL in mail (#254)

--- lib/Lufi/Controller/Mail.pm
+++ lib/Lufi/Controller/Mail.pm
@@ -5,6 +5,7 @@ use Mojo::JSON qw(decode_json);
 use Mojo::URL;
 use Email::Valid;
 use URI::Find;
+use URI::Find::Schemeless;
 
 sub render_mail {
     my $c = shift;
@@ -39,7 +40,7 @@ sub send_mail {
         my ($uri, $orig_uri) = @_;
         $uri = Mojo::URL->new($uri);
         if ($uri->host ne $base_url->to_abs->host && $uri->host ne $fixed_url->to_abs->host) {
-            $msg .= $c->l('You can\'t add URLs that are not related to this instance.').'<br>';
+            $msg .= $c->l('You can\'t add URLs that are not related to this instance (%1).', $orig_uri).'<br>';
         } elsif (index($orig_uri, $fixed_url->to_abs->to_string) > -1) {
             $at_least_one_instance_url = 1;
         }
@@ -48,7 +49,20 @@ sub send_mail {
     $finder->find(\$body);
     $finder->find(\$subject);
 
-    $c->debug($at_least_one_instance_url);
+    # Schemeless URI beginning with www, which are interpreted by mailers 🤦
+    $finder = URI::Find::Schemeless->new(sub {
+        my ($uri, $orig_uri) = @_;
+        return $orig_uri if ($uri !~ m/www/);
+
+        $uri = Mojo::URL->new($uri);
+        if ($uri->host ne $base_url->to_abs->host && $uri->host ne $fixed_url->to_abs->host) {
+            $msg .= $c->l('You can\'t add URLs that are not related to this instance (%1).', $orig_uri).'<br>';
+        }
+        return $orig_uri;
+    });
+    $finder->find(\$body);
+    $finder->find(\$subject);
+
     unless ($at_least_one_instance_url) {
         $msg .= $c->l('The body of the mail must contain at least one URL pointing to a file hosted on this instance.').'<br>';
     }
