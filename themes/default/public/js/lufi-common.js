ed7656bdc3f82b2d6fe3d65ebacb96156c25bb9a
Booteille <booteille@protonmail.com>
2024-06-26 15:56:04 +0200

    Use lufi-api and WebCrypto
    Apply 1 suggestion(s) to 1 file(s)
    
    Co-authored-by: Luc Didry <luc@framasoft.org>
    
    Apply 1 suggestion(s) to 1 file(s)
    
    Co-authored-by: Luc Didry <luc@framasoft.org>
    
    Replace Sjcl by Lufi API in readme
    
    Remove sjcl dependency
    
    Update lufi-api to last commit
    
    Do not b64 encode/decode since lufi-api handles that already. Do not read the file since it's not needed
    
    update Lufi API
    
    Formatting
    
    Build Lufi API with Bun v1.1.21 for performances improvements
    
    Working version with Lufi API
    
    Update lufi API to last version
    
    Update lufi API to last version

--- themes/default/public/js/lufi-common.js
+++ themes/default/public/js/lufi-common.js
@@ -15,7 +15,7 @@ function escapeHtml(string) {
     });
 }
 function changeLang() {
-    window.location = langUrl+$(this).val();
+    window.location = langUrl + $(this).val();
 }
 function formatDate(unixTimestamp) {
     return new Date(unixTimestamp * 1000).toLocaleString(window.navigator.language, {
@@ -27,22 +27,7 @@ function formatDate(unixTimestamp) {
         minute: '2-digit',
     })
 }
-function delItem(name) {
-    var files = localStorage.getItem(`${window.prefix}files`);
-    if (files === null) {
-        files = new Array();
-    } else {
-        files = JSON.parse(files);
-    }
-    var i;
-    for (i = 0; i < files.length; i++) {
-        if (files[i].short === name) {
-            files.splice(i, 1);
-        }
-    }
-    localStorage.setItem(`${window.prefix}files`, JSON.stringify(files));
-}
-$(document).ready(function(){
+$(document).ready(function () {
     $('select').material_select();
     $(".select-lang select").on('change', changeLang);
     $(".select-lang-mobile select").on('change', changeLang);
