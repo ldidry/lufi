8dfa3f1d7525eea93eeb2f0477f534030b09f48d
Booteille <booteille@protonmail.com>
2024-12-05 16:37:47 +0100

    Give focus to file upload on page load
    Remove upload input autofocus on page load

--- themes/default/public/js/upload.js
+++ themes/default/public/js/upload.js
@@ -329,6 +329,36 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   };
 
+  const showSmallUploadZone = () => {
+    document.getElementById("provided-files").classList.remove("is-hidden");
+    document.getElementById("upload-controls").classList.remove("is-hidden");
+    document
+      .querySelector("#file-js-upload .total-size")
+      .classList.remove("is-hidden");
+
+    document
+      .querySelector("#file-js-upload .small-version")
+      .classList.remove("is-hidden");
+    document
+      .querySelector("#file-js-upload .full-version")
+      .classList.add("is-hidden");
+  };
+
+  const showFullUploadZone = () => {
+    document.querySelector("#provided-files").classList.add("is-hidden");
+    document.getElementById("upload-controls").classList.add("is-hidden");
+    document
+      .querySelector("#file-js-upload .total-size")
+      .classList.add("is-hidden");
+
+    document
+      .querySelector("#file-js-upload .small-version")
+      .classList.add("is-hidden");
+    document
+      .querySelector("#file-js-upload .full-version")
+      .classList.remove("is-hidden");
+  };
+
   /**
    * Update the progress bar of the File Card
    *
@@ -475,33 +505,3 @@ document.addEventListener("DOMContentLoaded", () => {
       .append(maxSizeDOM);
   }
 });
-
-const showSmallUploadZone = () => {
-  document.getElementById("provided-files").classList.remove("is-hidden");
-  document.getElementById("upload-controls").classList.remove("is-hidden");
-  document
-    .querySelector("#file-js-upload .total-size")
-    .classList.remove("is-hidden");
-
-  document
-    .querySelector("#file-js-upload .small-version")
-    .classList.remove("is-hidden");
-  document
-    .querySelector("#file-js-upload .full-version")
-    .classList.add("is-hidden");
-};
-
-const showFullUploadZone = () => {
-  document.querySelector("#provided-files").classList.add("is-hidden");
-  document.getElementById("upload-controls").classList.add("is-hidden");
-  document
-    .querySelector("#file-js-upload .total-size")
-    .classList.add("is-hidden");
-
-  document
-    .querySelector("#file-js-upload .small-version")
-    .classList.add("is-hidden");
-  document
-    .querySelector("#file-js-upload .full-version")
-    .classList.remove("is-hidden");
-};
